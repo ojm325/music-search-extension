c1f8e6efc2e366dc796d9c052746014a4acb485e
Omar Martinez <omar.martinez325@gmail.com>
2015-07-15 03:46:01 -0400

    - Added visual data to the popup page (the term that was searched, album title, album cover)

--- musicSearch.js
+++ musicSearch.js
@@ -1,4 +1,8 @@
-document.write("You searched for: " +chrome.extension.getBackgroundPage().selectedText);
+//document.write("You searched for: " +chrome.extension.getBackgroundPage().selectedText);
+$(function(){
+    $('<h2>').appendTo('#searchedTerm').append(chrome.extension.getBackgroundPage().selectedText);
+});
+
 searchForAlbums(chrome.extension.getBackgroundPage().selectedText);
 
 function searchForAlbums(query){
@@ -9,9 +13,27 @@ function searchForAlbums(query){
             type: 'album'
         },
         success: function (response) {
-			$.each(response["albums"], function(key, value){
-				document.write("Check console logs for response");
+            var count = 0;
+			$.each(response["albums"]["items"], function(key, value){
+				//document.write("Check console logs for response");
 			    console.log(key, value);
+
+                var album = response["albums"]["items"][count];
+                $(function(){
+                    var container = $('<div>', 
+                        {
+                            class:'album',
+                            id: response["albums"]["items"][count]["id"]
+                        });
+                    container.appendTo('#albumList');
+
+                    $('<h3>').appendTo('.album').append(album["name"]);
+                    $('<img />', {
+                        src: album['images'][1]['url'],
+                    }).appendTo('.album');
+                });
+
+                count++;
 			});
         }
     });
