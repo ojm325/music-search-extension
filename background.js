6b804f8f967452f19362e13b057564d349fb9ec8
Omar Martinez <omar.martinez325@gmail.com>
2015-07-16 23:55:04 -0400

    - Plugin no longer opens up a new tab, but rather opens up a window within the tab - Added the ability to hover over albums - Code isn't working for some reason.

--- background.js
+++ background.js
@@ -1,23 +1,32 @@
-chrome.contextMenus.create({"title": "Search For This Music",
-                                      "contexts":["selection"]});
+ $(function(){
+  chrome.contextMenus.create({"title": "Search For This Music",
+                                        "contexts":["selection"]});
 
-chrome.contextMenus.onClicked.addListener(genericOnClick);
+  chrome.contextMenus.onClicked.addListener(function(info, tab) {
+      if (tab) {
+          
+          var code = [
+              'var d = document.createElement("div");',
+              "d.setAttribute('style', '"
+                  + "background-color: blue; "
+                  + "width: 300px; "
+                  + "height: 300px; "
+                  + "position: fixed; "
+                  + "top: 70px; "
+                  + "left: 30px; "
+                  + "overflow: auto;"
+                  + "z-index: 9999; "
+                  + "');",
+              "d.setAttribute('id', 'musicSearchContainer');",
+              'document.body.appendChild(d);'
+          ].join("\n");
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    if (request.method == "getSelection")
-      sendResponse({data: window.getSelection().toString()});
-    else
-      sendResponse({}); 
-});
 
-
-function genericOnClick(info, tab) {
-  selectedText = info.selectionText;
-
-
-  chrome.tabs.create({url: "popup.html"}, function(tab) {
-    chrome.tabs.executeScript(tab.id, {file: "musicSearch.js"}, function() {
-    });
+          chrome.tabs.executeScript(tab.id, { file: "jquery-2.1.4.min.js" }, function(){});
+          chrome.tabs.executeScript(tab.id, { file: "jquery-ui.js" }, function(){});
+          chrome.tabs.executeScript(tab.id, {file: "musicSearch.js"}, function() {
+          });
+      }
   });
-  
-}
+
+});
--- musicSearch.js
+++ musicSearch.js
@@ -1,25 +1,30 @@
-//document.write("You searched for: " +chrome.extension.getBackgroundPage().selectedText);
 $(function(){
-    $('<h2>').appendTo('#searchedTerm').append(chrome.extension.getBackgroundPage().selectedText);
-});
+    $( "#musicSearchContainer" ).draggable();
+    $('<div>', {id:'searchedTerm'}).appendTo('#musicSearchContainer');
 
-searchForAlbums(chrome.extension.getBackgroundPage().selectedText);
+    //$('<h2>').appendTo('#searchedTerm').append(chrome.extension.getBackgroundPage().selectedText);
+    $('<h2>').appendTo('#searchedTerm').append("Swans");
 
-function searchForAlbums(query){
-	$.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-            q: query,
-            type: 'album'
-        },
-        success: function (response) {
-            var count = 0;
-			$.each(response["albums"]["items"], function(key, value){
-				//document.write("Check console logs for response");
-			    console.log(key, value);
+    searchForAlbums("Swans");
 
-                var album = response["albums"]["items"][count];
-                $(function(){
+    function searchForAlbums(query){
+        $('<div>', {id:'albumTitle'}).appendTo('#musicSearchContainer');
+        $('<div>', {id:'albumList'}).appendTo('#musicSearchContainer');
+
+    	$.ajax({
+            url: 'https://api.spotify.com/v1/search',
+            data: {
+                q: query,
+                type: 'album'
+            },
+            success: function (response) {
+                var count = 0;
+    			$.each(response["albums"]["items"], function(key, value){
+    				//document.write("Check console logs for response");
+    			    console.log(key, value);
+
+                    var album = response["albums"]["items"][count];
+
                     var container = $('<div>', 
                         {
                             class:'album',
@@ -31,10 +36,18 @@ function searchForAlbums(query){
                     $('<img />', {
                         src: album['images'][1]['url'],
                     }).appendTo('.album');
-                });
 
-                count++;
-			});
-        }
-    });
-}
+                    $( ".album" ).hover(function() {
+                        $( '#albumTitle' ).text("HI!");
+                    }, function() {
+                        $( '#albumTitle' ).text("");
+                    });
+
+
+                    count++;
+    			});
+            }
+        });
+    }
+
+});
